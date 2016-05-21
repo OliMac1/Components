02ef357fd51612bed1eeccb239f3b5f70d0fb28e
Oliver McLeod <olivermcleod96@gmail.com>
2016-05-21 20:02:24 +0100

    map map forEach test end

--- src/com/mcleodog/Components/Main.java
+++ src/com/mcleodog/Components/Main.java
@@ -9,8 +9,12 @@ public class Main {
 
     public static void main(String[] args){
         HashMap<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1,"Wow");
+        map.put(12,"god");
+        map.put(2,"you");
         map.forEach((k,v) -> {
-
+            System.out.println(v);
         });
+        System.out.println("End");
     }
 }
