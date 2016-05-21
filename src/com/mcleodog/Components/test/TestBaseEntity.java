f1b4978e20413eec58ff8815062d261aba20e45b
Oliver McLeod <olivermcleod96@gmail.com>
2016-05-21 22:51:08 +0100

    IBaseEntity added

--- src/com/mcleodog/Components/test/TestBaseEntity.java
+++ src/com/mcleodog/Components/test/TestBaseEntity.java
@@ -1,5 +1,6 @@
-package com.mcleodog.Components;
+package com.mcleodog.Components.test;
 
+import com.mcleodog.Components.IComponent;
 import com.mcleodog.Components.exceptions.NullComponentException;
 
 import java.util.HashMap;
@@ -7,11 +8,11 @@ import java.util.HashMap;
 /**
  * Created by olivermcleod on 21/05/2016.
  */
-public class BaseEntity {
+public class TestBaseEntity {
 
     private HashMap<Integer, IComponent> components;
 
-    public BaseEntity(){
+    public TestBaseEntity(){
         components = new HashMap<Integer, IComponent>();
     }
 
