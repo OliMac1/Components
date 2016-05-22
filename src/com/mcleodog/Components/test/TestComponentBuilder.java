29cd2c5861ee2e1f961869453a377a9b3efc5ca9
Oliver McLeod <olivermcleod96@gmail.com>
2016-05-22 22:39:52 +0100

    reflections library added

--- src/com/mcleodog/Components/test/TestComponentBuilder.java
+++ src/com/mcleodog/Components/test/TestComponentBuilder.java
@@ -6,13 +6,13 @@ import com.mcleodog.Components.annotations.AnnComponentBuilder;
 import com.mcleodog.Components.io.Loading;
 
 import java.io.ByteArrayInputStream;
-import java.nio.file.Path;
 
 /**
  * Created by olivermcleod on 22/05/2016.
  */
 @AnnComponentBuilder(name = "TestComponent", value=1)
 public class TestComponentBuilder implements IComponentBuilder{
+
     @Override
     public IComponent buildNew() {
         return new TestComponent(0);
