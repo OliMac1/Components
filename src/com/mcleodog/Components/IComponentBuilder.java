3b7478fa4e71a6590831f9727315bc190ace6dab
Oliver McLeod <olivermcleod96@gmail.com>
2016-05-22 20:37:29 +0100

    pre-Annotation test

---
+++ src/com/mcleodog/Components/IComponentBuilder.java
@@ -0,0 +1,11 @@
+package com.mcleodog.Components;
+
+/**
+ * Created by Oliver on 22/05/2016.
+ */
+
+public interface IComponentBuilder {
+
+    IComponent buildNew();
+    IComponent buildFromSave();
+}
