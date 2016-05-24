d0a406ae3b5d076c6122ff67733a4d250d2016dc
Oliver McLeod <olivermcleod96@gmail.com>
2016-05-24 19:55:13 +0100

    todo added to ComponentIdentHandler

--- src/com/mcleodog/Components/ComponentIdentLoader.java
+++ src/com/mcleodog/Components/ComponentIdentLoader.java
@@ -3,6 +3,7 @@ package com.mcleodog.Components;
 /**
  * Created by olivermcleod on 23/05/2016.
  */
+//TODO sort out ident handling.
 public class ComponentIdentLoader {
 
     private static int i = 0;
